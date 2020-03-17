1717db4bf5c39532994e5a182d48e3cc5518988d
James Baker <bakerje@mac.com>
2020-03-17 19:33:06 +0000

    Added video thumbs and links

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term
-      }
+        }
     });
 
     this.setState({ videos: response.data.items });
-  }
+
+  };
+
+
+  onVideoSelect = (video) => {
+    console.log(video);
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos.
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
-    )
+    );
+    }
   }
-}
+
 
 export default App;
